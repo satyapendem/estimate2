01ca32b001079c1e1b73c8685783c888a0a0c2cb
Hardik Khandelwal <hardikk.int@homelane.com>
2019-07-04 16:59:34 +0530

    single api

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 from joblib import dump, load
 import numpy as np
 import timeit
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -29,7 +30,6 @@ lrB = load('./models/lrB.joblib')
 lrC = load('./models/lrC.joblib')
 end= timeit.timeit()
 
-#print("time to load 6 models for kitchen", end-start," seconds")
 
 #Loading Models for bedroom
 columns_bedroom =['Property Config_1 BHK', 'Property Config_2 BHK',
@@ -50,6 +50,9 @@ lr_bedroom1 = load('./models/lr_bedroom1.joblib')
 lr_bedroom2 = load('./models/lr_bedroom2.joblib')
 lr_bedroom3= load('./models/lr_bedroom3.joblib')
 end=timeit.timeit()
+
+## Loading csv to compute kitchen shape
+kitchen_shape_entries = pd.read_csv('./kitchen_shape.csv')
 
 
 
@@ -77,6 +80,13 @@ def get_city(data):
     else:
         return city
 
+def get_kitchen_shape(data):
+    city=get_city(data)
+    bin=get_bin(data['floorplan_size'])
+    property_config= data['property_config']
+
+    temp = kitchen_shape_entries.loc[(kitchen_shape_entries['property_config']==property_config) & (kitchen_shape_entries['bin']==bin) & (kitchen_shape_entries['city']==city)]
+    return temp['kitchen_shape'].iloc[0]
 
 #Function to return input vector for model
 def get_input_vector_kitchen(data):
@@ -123,104 +133,60 @@ def get_input_vector_bedroom(data):
     model_input[feature_index_bedroom[str(bin)]]=1
     return model_input
 
+def predict_kitchen(data):
+    list_attributes=['city','floorplan_size','property_config']
+    for i in list_attributes:
+        if(i not in data):
+            return "Mandatory Params missing"
+
+    kitchen_shape = get_kitchen_shape(data)
+
+    model_input = get_input_vector_kitchen(data)
+    wallA = lrA.predict(model_input.reshape(1,-1))
+    wallA=wallA[0]
+    wallB = lrB.predict(model_input.reshape(1,-1))
+    wallB=wallB[0]
+    wallC = lrC.predict(model_input.reshape(1,-1))
+    wallC=wallC[0]
+    return {'wall_a':wallA,'wall_b':wallB,'wall_c':wallC,'kitchen_shape':kitchen_shape}
+
+def predict_bedroom(data):
+    list_attributes=['city','floorplan_size','property_config']
+    for i in list_attributes:
+        if(i not in data):
+            return "Mandatory Params missing"
 
 
-@app.route("/kitchen/random_forest",methods=['POST'])
-def predict_rf():
-    try:
-        data  = request.get_json()
-        print(data)
-        list_attributes=['city','floorplan_size','property_config']
-        for i in list_attributes:
-            if(i not in data):
-                return "Mandatory Params missing"
+    property_config = data['property_config']
 
-        model_input = get_input_vector_kitchen(data)
-        wallA = rfA.predict(model_input.reshape(1,-1))
-        wallA=wallA[0]
-        wallB = rfB.predict(model_input.reshape(1,-1))
-        wallB=wallB[0]
-        wallC = rfC.predict(model_input.reshape(1,-1))
-        wallC=wallC[0]
-
-        return jsonify({'wall_a':wallA,'wall_b':wallB,'wall_c':wallC})
-    except:
-        return jsonify({'msg':"Unexpected Input or Internal Server error"})
-
-@app.route("/kitchen/linear_regression",methods=['POST'])
-def predict_lr():
-    try:
-        data  = request.get_json()
-        list_attributes=['city','floorplan_size','property_config']
-        for i in list_attributes:
-            if(i not in data):
-                return "Mandatory Params missing"
-        model_input = get_input_vector_kitchen(data)
-        wallA = lrA.predict(model_input.reshape(1,-1))
-        wallA=wallA[0]
-        wallB = lrB.predict(model_input.reshape(1,-1))
-        wallB=wallB[0]
-        wallC = lrC.predict(model_input.reshape(1,-1))
-        wallC=wallC[0]
-        return jsonify({'wall_a':wallA,'wall_b':wallB,'wall_c':wallC})
-    except:
-        return jsonify({'msg':"Unexpected Input or Internal Server error"})
-
-@app.route("/bedroom/random_forest",methods=['POST'])
-def predict_bedroom_rf():
-    try:
-        data  = request.get_json()
-        list_attributes=['city','floorplan_size','property_config']
-        for i in list_attributes:
-            if(i not in data):
-                return "Mandatory Params missing"
+    model_input = get_input_vector_bedroom(data)
+    wallA=0
+    wallB=0
+    wallC=0
+    wallA = lr_bedroom1.predict(model_input.reshape(1,-1))
+    wallA=wallA[0][0]
+    if(property_config!='1 BHK'):
+        wallB = lr_bedroom2.predict(model_input.reshape(1,-1))
+        wallB=wallB[0][0]
+    if(property_config!='1 BHK' and property_config!='2 BHK'):
+        wallC = lr_bedroom3.predict(model_input.reshape(1,-1))
+        wallC=wallC[0][0]
+    return {'bedroom_1':wallA,'bedroom_2':wallB,'bedroom_3':wallC}
 
 
-        property_config = data['property_config']
-
-        model_input = get_input_vector_bedroom(data)
-        wallA=0
-        wallB=0
-        wallC=0
-        wallA = rf_bedroom1.predict(model_input.reshape(1,-1))
-        wallA=wallA[0]
-        if(property_config!='1 BHK'):
-            wallB = rf_bedroom2.predict(model_input.reshape(1,-1))
-            wallB=wallB[0]
-        if(property_config!='1 BHK' and property_config!='2 BHK'):
-            wallC = rf_bedroom3.predict(model_input.reshape(1,-1))
-            wallC=wallC[0]
-        # print(data['city'])
-        return jsonify({'bedroom_1':wallA,'bedroom_2':wallB,'bedroom_3':wallC})
-    except:
-        return jsonify({'msg':"Unexpected Input or Internal Server error"})
-
-
-@app.route("/bedroom/linear_regression",methods=['POST'])
-def predict_bedroom_lr():
+@app.route("/predict",methods=['POST'])
+def predict():
     try:
         data  = request.get_json()
         list_attributes=['city','floorplan_size','property_config']
         for i in list_attributes:
             if(i not in data):
-                return "Mandatory Params missing"
+                return jsonify({'msg':"Mandatory Params missing"})
+        kitchen_output= predict_kitchen(data)
+        bedroom_output= predict_bedroom(data)
 
 
-        property_config = data['property_config']
-
-        model_input = get_input_vector_bedroom(data)
-        wallA=0
-        wallB=0
-        wallC=0
-        wallA = lr_bedroom1.predict(model_input.reshape(1,-1))
-        wallA=wallA[0][0]
-        if(property_config!='1 BHK'):
-            wallB = lr_bedroom2.predict(model_input.reshape(1,-1))
-            wallB=wallB[0][0]
-        if(property_config!='1 BHK' and property_config!='2 BHK'):
-            wallC = lr_bedroom3.predict(model_input.reshape(1,-1))
-            wallC=wallC[0][0]
-        return jsonify({'bedroom_1':wallA,'bedroom_2':wallB,'bedroom_3':wallC})
+        return jsonify({'kitchen':kitchen_output,'bedroom':bedroom_output})
     except:
         return jsonify({'msg':"Unexpected Input or Internal Server error"})
 
---
+++ generate_kitchen_shape.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('./Estimator 2.0 Data Analysis - visualize_kitchen_shape.csv')
+#removing very high floor plan sizes( Only 1 sample)
+df=df[df['Floorplan Size']<=5000]
+#Binning by quantiles
+bins = [287,1055,1233,1585,4813]
+df['binned'] = pd.cut(df['Floorplan Size'],bins=bins,labels=[1,2,3,4])
+df_new = pd.DataFrame()
+df_new['Property Config']= df['Property Config']
+df_new['Kitchen Shape'] = df['Kitchen Shape']
+df_new['City'] =df['City']
+df_new['Floorplan Size']= df['Floorplan Size']
+df_new['Wall A']=df['Wall A']
+df_new['Wall B']=df['Wall B']
+df_new['Wall C']=df['Wall C']
+df_new['binned']=df['binned']
+#df_new = df_new[df_new['City']!='Kolkata']
+#df_new = df_new[df_new['City']!='Pune']
+#df_new = df_new[df_new['Property Config']!='4 BHK']
+df_new.loc[df['City']=='Thane',"City"]='Mumbai'
+df_new.loc[df['City']=='Thane',"City"]='Mumbai'
+df_new.loc[df['City'].isin(['Gurgaon', 'Ghaziabad','New Delhi','Noida']),"City"]='NCR'
+df_new.loc[df['City']=='Chennai',"City"]='Chennai'
+df_new.loc[df['City']=='Hyderabad',"City"]='Hyderabad'
+
+binned=np.unique(df_new['binned'])
+city=np.unique(df_new['City'])
+property_config = np.unique(df_new['Property Config'])
+
+list_entries=[]
+for b in binned:
+    for c in city:
+        for p in property_config:
+            df_temp = df_new.loc[df_new['Property Config']==p]
+            df_temp = df_temp.loc[df_temp['City']==c]
+            df_temp= df_temp.loc[df_temp['binned']==b]
+            mode= df_temp['Kitchen Shape'].mode()
+            if(len(mode)==0):
+                mode='l-shaped'
+            else:
+                mode=mode[0]
+            d_temp = {'property_config':p,'city':c,'bin':b,'kitchen_shape':mode}
+            list_entries.append(d_temp)
+df_entries = pd.DataFrame(list_entries)
+df_entries.to_csv('./kitchen_shape.csv')
